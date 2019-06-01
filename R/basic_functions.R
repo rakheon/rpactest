9f1a4502c66fe18f93d21991040986173f06bb19
rakheon <rakheon.kim@gmail.com>
2019-06-01 16:44:37 -0500

    added func des

--- R/basic_functions.R
+++ R/basic_functions.R
@@ -1,4 +1,14 @@
 # Vector soft-thresholding operator
+#' Title
+#'
+#' @param a - aa
+#' @param lambda - bb
+#'
+#' @return st value
+#' @export
+#'
+#' @examples
+#' soft_thresh(3,1)
 soft_thresh <- function(a, lambda){
     return(as.numeric(sign(a)*pmax(0,abs(a) - lambda)))
 }
